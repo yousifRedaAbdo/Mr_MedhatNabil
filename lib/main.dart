04facc4eea4bd15dcfd1337db40eedb532c8e752
YousifReda <yousifreda2001@gmail.com>
2023-12-12 20:51:41 +0200

    from new branch

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,13 @@
 import 'dart:io';
 
-import 'package:easy_localization/easy_localization.dart';
+ import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 import 'locator.dart';
 import 'routs/app_router.dart';
 import 'services/navigation_service.dart';
-
+//----------------------------------------------------------------------------------------------------------
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await EasyLocalization.ensureInitialized();
